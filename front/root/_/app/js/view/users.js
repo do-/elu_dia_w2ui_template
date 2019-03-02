3da59be43f8a891594b7741c24ce1d49e13a0e22
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-02 17:30:14 +0300

    copied code from https://github.com/6pac/SlickGrid/blob/master/examples/example6-ajax-loading.html

--- front/root/_/app/js/view/users.js
+++ front/root/_/app/js/view/users.js
@@ -3,6 +3,9 @@ define ([], function () {
     return function (data, view) {
     
         $('title').text ('Пользователи системы')
+        
+        fill (view, data, $('main'))        
+        
 /*
         $('main').w2regrid ({ 
         
@@ -29,6 +32,88 @@ define ([], function () {
         
         $('#grid_usersGrid_search_all').focus ()
 */
+
+    var grid, s;
+    var loader = new Slick.Data.RemoteModel();
+    var mpnFormatter = function (row, cell, value, columnDef, dataContext) {
+        s ="<a href='" + dataContext.octopart_url + "' target=_blank>" + dataContext.mpn + "</a>";
+        return s;
+    };
+    var brandFormatter = function (row, cell, value, columnDef, dataContext) {
+        return dataContext.brand.name;
+    };
+    var columns = [
+        {id: "mpn", name: "MPN", field: "mpn", formatter: mpnFormatter, width: 100, sortable: true },
+        {id: "brand", name: "Brand", field: "brand.name", formatter: brandFormatter, width: 100, sortable: true },
+        {id: "short_description", name: "Description", field: "short_description", width: 520 },
+    ];
+    var options = {
+        rowHeight: 21,
+        editable: false,
+        enableAddRow: false,
+        enableCellNavigation: false,
+        enableColumnReorder: false,
+    };
+    
+    var loadingIndicator = null;
+    
+    $(function () {
+    
+        grid = new Slick.Grid("#myGrid", loader.data, columns, options);
+        
+        grid.onViewportChanged.subscribe(function (e, args) {
+            var vp = grid.getViewport();
+            loader.ensureData(vp.top, vp.bottom);
+        });
+        
+        grid.onSort.subscribe(function (e, args) {
+            loader.setSort(args.sortCol.field, args.sortAsc ? 1 : -1);
+            var vp = grid.getViewport();
+            loader.ensureData(vp.top, vp.bottom);
+        });
+        
+        loader.onDataLoading.subscribe(function () {
+            if (!loadingIndicator) {
+                loadingIndicator = $("<span class='loading-indicator'><label>Buffering...</label></span>").appendTo(document.body);
+                var $g = $("#myGrid");
+                loadingIndicator
+                        .css("position", "absolute")
+                        .css("top", $g.position().top + $g.height() / 2 - loadingIndicator.height() / 2)
+                        .css("left", $g.position().left + $g.width() / 2 - loadingIndicator.width() / 2);
+            }
+            loadingIndicator.show();
+        });
+        
+        loader.onDataLoaded.subscribe(function (e, args) {
+            for (var i = args.from; i <= args.to; i++) {
+                grid.invalidateRow(i);
+            }
+            grid.updateRowCount();
+            grid.render();
+            loadingIndicator.fadeOut();
+        });
+        
+        $("#txtSearch").keyup(function (e) {
+            if (e.which == 13) {
+                loader.setSearch($(this).val());
+                var vp = grid.getViewport();
+                loader.ensureData(vp.top, vp.bottom);
+            }
+        });
+        
+        loader.setSearch($("#txtSearch").val());
+        
+        loader.setSort("score", -1);
+        
+        grid.setSortColumn("score", false);
+        
+        // load the first page
+        
+        grid.onViewportChanged.notify();
+        
+    })
+
+
     }
 
 })
