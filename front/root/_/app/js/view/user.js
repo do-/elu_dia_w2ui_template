8e2baac214dfab7e57e317481be5c739ef9e7fdd
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-04 21:05:40 +0300

    cleanup

--- front/root/_/app/js/view/user.js
+++ front/root/_/app/js/view/user.js
@@ -1,17 +1,5 @@
 $_DRAW.user = async function (data) {
 
-    let __read_only = 1
-    
-    $_F5 = () => {
-    
-            $('.toolbar button').each (function () {
-                let $this = $(this)
-                let is_ro = $this.is ('[data-edit]') ? 0 : 1
-                $this.css ({display: is_ro == __read_only ? 'inline-block' : 'none'})
-            })
-            
-    }
-
     $('title').text (data.label)
     
     let $main = $('main').html ((await use.jq ('user')).draw_form (data))
@@ -19,5 +7,5 @@ $_DRAW.user = async function (data) {
     show_block ('user_options')
 
     return $main
-    
+
 }
