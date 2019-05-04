7098eb9fec0d6e45dc41f29d0e60799649c6fa45
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-04 21:56:27 +0300

    bg img renamed

--- front/root/_/app/js/view/login.js
+++ front/root/_/app/js/view/login.js
@@ -1,6 +1,6 @@
 $_DRAW.login = async function (data) {
 
-    let $body = $('body').html (await use.html ('login')).css ({backgroundImage: 'url(' + staticURL ('libs/_/img/The-Good-Times-1.JPG') + ')'})
+    let $body = $('body').html (await use.html ('login')).css ({backgroundImage: 'url(' + staticURL ('libs/_/img/bg.jpg') + ')'})
     
     $('input[name=login]').keypress    (function (e) {if (e.which == 13) $('input[name=password]').focus ()}).focus ()
     $('input[name=password]').keypress (function (e) {if (e.which == 13) $_DO.execute_login ()})
