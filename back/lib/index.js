14cfcb16356652260d5712e3471e16e7011becf7
Pavel Kudriavtsev <pik@reginc.pro>
2019-10-17 15:56:56 +0300

    remove_nginx_dependency

--- back/lib/index.js
+++ back/lib/index.js
@@ -1,7 +1,7 @@
 const conf = new (require ('./Config.js'));
 
 (async function () {
-    
+
     try {
         await conf.init ()
     }
@@ -9,17 +9,30 @@ const conf = new (require ('./Config.js'));
         return darn (['Initialization failed', x])
     }
 
+    let webUiFrontend;
+
+    if (conf.docroot) {
+        webUiFrontend = new (require ('./Content/Handler/WebUiFrontend.js')) (conf.docroot)
+    }
+
     require ('http').createServer (
-        
-        (request, response) => {new (require ('./Content/Handler/WebUiBackend.js')) ({
-        
-            conf, 
-            
-            pools: conf.pools, 
-            
-            http: {request, response}
-            
-        }).run ()}
+
+        (request, response) => {
+
+            webUiFrontend && request.url.substring (0, 6) != '/_back' ?
+
+                webUiFrontend.run (request, response) :
+
+                new (require ('./Content/Handler/WebUiBackend.js')) ({
+
+                    conf,
+
+                    pools: conf.pools,
+
+                    http: {request, response}
+
+                }).run ()
+        }
 
     ).listen (conf.listen, function () {darn ('default app is listening to HTTP at ' + this._connectionKey)})
 
