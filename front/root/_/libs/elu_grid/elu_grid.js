a6c0076753d214c294fe3fa1dcdbce3f4ac0fae0
Dmitry Ovsyanko <do_@pochta.ru>
2020-03-24 17:57:45 +0300

    popup close

--- front/root/_/libs/elu_grid/elu_grid.js
+++ front/root/_/libs/elu_grid/elu_grid.js
@@ -558,7 +558,30 @@ $.fn.draw_popup = async function (o) {
 	let $header = $('<header>').appendTo ($div)
 	
 	$('<span>').text (this.attr ('title')).appendTo ($header)
-	$('<button>').appendTo ($header)
+	
+	let close = () => {
+
+		$div.remove ()
+
+		if ($('.elu_popup').length == 0) $('.elu_lock').remove ()
+
+	}
+	
+	let confirm_close = (e) => {
+
+		if (e.keyCode != 27 || e.ctrlKey || e.altKey || !confirm ('Закрыть окно диалога?')) return
+
+		close ()
+		
+		$body.off ('keyup', confirm_close)
+		
+		blockEvent (e)
+	
+	}
+
+	$body.on ('keyup', confirm_close)
+	
+	$('<button>').appendTo ($header).click (close)
 	
 	$div.append (this).css ({visibility: 'hidden'})
 
