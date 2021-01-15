c601699c6d8e37f428ec0dbf3b3964a864b7ba4d
Dmitry Ovsyanko <do_@pochta.ru>
2021-01-15 11:54:16 +0300

    HttpRouter FIX (used to dispatch keep alive requests to static handler, leading to session expiry)

--- back/lib/HttpRouter.js
+++ back/lib/HttpRouter.js
@@ -4,8 +4,10 @@ const front = require ('./Ext/Dia/Content/Handler/HTTP/EluStatic.js')
 module.exports = class extends require ('./Ext/Dia/Content/Handler/HTTP/Router.js') {
 
 	create_http_handler (http) {
+	
+		let {method, url} = http.request
 
-		if (http.request.url.match (/^\/(\?|_back)/)) return new back ({conf: this.conf, pools: this.conf.pools, http})
+		if (method == 'POST' || url.match (/^\/(\?|_back)/)) return new back ({conf: this.conf, pools: this.conf.pools, http})
 
 		return new front ({http})
 
