7bbc73f938af632d1bdd723ac5f2725135fb3358
Dmitry Ovsyanko <do_@pochta.ru>
2021-01-20 16:47:27 +0300

    Config: using base Dia.Config

--- back/lib/Config.js
+++ back/lib/Config.js
@@ -1,17 +1,14 @@
-const fs  = require ('fs')
 const Dia = require ('./Ext/Dia/Dia.js')
 
-module.exports = class {
+module.exports = class extends Dia.Config {
 
     constructor () {
-
-        const conf = JSON.parse (fs.readFileSync ('../conf/elud.json', 'utf8'))
-
-        for (let k in conf) this [k] = conf [k]
+    
+    	super ('../conf/elud.json')
                 
         this.pools = {
         
-        	db       : Dia.DB.Pool (this.db, new (require ('./Model.js')) ({path: './Model'})),
+        	db       : Dia.DB.Pool (this.db, new (require ('./Model.js')) ()),
         	
             sessions : this.setup_sessions (),
             
@@ -44,7 +41,7 @@ module.exports = class {
 		await db.load_schema ()
 		
 		await db.update_model ()
-		
+
     }
 
 }
